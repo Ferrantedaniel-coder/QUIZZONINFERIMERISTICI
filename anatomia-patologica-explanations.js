45a190a5267f7ebca89a76e6907e866cfb42bedd
Ferrantedaniel-coder <Ferrantedaniel@outlook.it>
2026-09-15 00:08:27 +0200

    Load Anatomia Patologica explanations through 160

--- anatomia-patologica-explanations.js
+++ anatomia-patologica-explanations.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-const OPTIONAL_EXPLANATION_FILES=['data/anatomia-patologica-explanations-001.json','data/anatomia-patologica-explanations-002.json','data/anatomia-patologica-explanations-003.json'];
-const EXPECTED_ADVANCED=120;
+const OPTIONAL_EXPLANATION_FILES=['data/anatomia-patologica-explanations-001.json','data/anatomia-patologica-explanations-002.json','data/anatomia-patologica-explanations-003.json','data/anatomia-patologica-explanations-004.json'];
+const EXPECTED_ADVANCED=160;
 const ADVANCED_EXPLANATIONS={};
 let feedbackObserver=null;
 
