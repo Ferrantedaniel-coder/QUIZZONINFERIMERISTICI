991927d07454f144ae6d07514f499fffd48ef8f3
Ferrantedaniel-coder <Ferrantedaniel@outlook.it>
2026-09-15 00:36:33 +0200

    Load final Anatomia Patologica explanation block

--- anatomia-patologica-explanations.js
+++ anatomia-patologica-explanations.js
@@ -1,7 +1,7 @@
 (function(){
 'use strict';
-const OPTIONAL_EXPLANATION_FILES=['data/anatomia-patologica-explanations-001.json','data/anatomia-patologica-explanations-002.json','data/anatomia-patologica-explanations-003.json','data/anatomia-patologica-explanations-004.json','data/anatomia-patologica-explanations-005.json','data/anatomia-patologica-explanations-006.json','data/anatomia-patologica-explanations-007.json'];
-const EXPECTED_ADVANCED=280;
+const OPTIONAL_EXPLANATION_FILES=['data/anatomia-patologica-explanations-001.json','data/anatomia-patologica-explanations-002.json','data/anatomia-patologica-explanations-003.json','data/anatomia-patologica-explanations-004.json','data/anatomia-patologica-explanations-005.json','data/anatomia-patologica-explanations-006.json','data/anatomia-patologica-explanations-007.json','data/anatomia-patologica-explanations-008.json'];
+const EXPECTED_ADVANCED=300;
 const ADVANCED_EXPLANATIONS={};
 let feedbackObserver=null;
 
